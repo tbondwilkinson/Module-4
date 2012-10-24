2056cbbddcb9f9c795c14beb18bf975e2b3fa62d
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-23 20:58:49 -0500

    a

--- Calendar/main.php
+++ Calendar/main.php
@@ -29,6 +29,23 @@
 <div id="bucket"></div>
 
 <script type="text/javascript">
+function getCenteredCoords(width, height) {
+    var xPos = null;
+    var yPos = null;
+    if (window.ActiveXObject) {
+        xPos = window.event.screenX - (width/2) + 100;
+        yPos = window.event.screenY - (height/2) - 100;
+    } else {
+        var parentSize = [window.outerWidth, window.outerHeight];
+        var parentPos = [window.screenX, window.screenY];
+        xPos = parentPos[0] +
+            Math.max(0, Math.floor((parentSize[0] - width) / 2));
+        yPos = parentPos[1] +
+            Math.max(0, Math.floor((parentSize[1] - (height*1.25)) / 2));
+    }
+   return [xPos, yPos];
+}
+
 function openPopupWindow(openid) {
   document.getElementById('ops').style.display = 'none';
   document.getElementById('bucket').innerHTML = 'Signing you in <img src="/static/spinner.gif"/>';
