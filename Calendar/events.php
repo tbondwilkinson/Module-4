35f90c836c3436124ae4b78582a3acfc16075bc1
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-24 18:19:19 -0500

    t

--- Calendar/events.php
+++ Calendar/events.php
@@ -24,7 +24,7 @@ $stmt->bind_result($name, $datetime);
 
 while($stmt->fetch()) {
 	echo "{\n";
-	echo "title: " . $name . ",\n";
+	echo "title: '" . $name . "',\n";
 	$php_date_str = substr($datetime, 0, 4). ", " . substr($datetime,5,2) . ", " . substr($datetime, 8, 2) . ", " . substr($datetime, 11, 2) . ", " . substr($datetime, 14, 2);
 	echo "start: new Date(" . $php_date_str . ")\n";
 	echo "}\n";
