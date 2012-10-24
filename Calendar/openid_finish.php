53fa4248ca02523141e304b84b5a904e1f23276f
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-23 21:45:17 -0500

    t

--- Calendar/openid_finish.php
+++ Calendar/openid_finish.php
@@ -2,28 +2,9 @@
 
 	session_start();
 
-	print_r($_GET);
-
 	$usid = @$_SESSION['identifier'] ?: null;
 	unset($_SESSION['identifier']);
- 
-	$queryString = count($_POST) ? file_get_contents('php://input') : $_SERVER['QUERY_STRING'];
- 
-	$message = new OpenID_Message($queryString, OpenID_Message::FORMAT_HTTP);
- 
-	$result = $o->verify(new Net_URL2($returnTo . '?' . $queryString), $message);
- 
-	if($result->success()){
-		// Login Success!
- 
-		// Get the OpenID identifier, which is unique to every OpenID user (i.e. you can use it in your database to
-		// keep track of people between logins), and save it in the session:
-		$_SESSION["openid.identity"] = $message->get("openid.identity");
- 
-		// Now redirect to the target page for logged-in users
-	}else{
-		// Login Failed.  You can redirect back to the login page or whatever
-	}
 
+	$_SESSION['identifier'] = $_GET["openid_identity"];
 	exit;
 ?>
