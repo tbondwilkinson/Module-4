b6f56aedb80078ba9a25de0c8c1e2f7f3d24e57c
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-24 12:11:12 -0500

    t

--- Calendar/main.php
+++ Calendar/main.php
@@ -75,13 +75,56 @@ function openGoogleWindow() {
 function handleOpenIDResponse(openid_args) {
   document.getElementById('ops').style.display = 'none';
   document.getElementById('bucket').innerHTML = 'Verifying OpenID response';
-  YAHOO.util.Connect.asyncRequest('GET', './openid_finish.php?'+openid_args,
-      {'success': function() {
-      		alert("success"); 
-      		document.getElementById("bucket").innerHTML = "SUCCESSSS";
 
-      		$("#ops").show();
-         }}); 
+  <?php
+  	require "database.php";
+  	session_start();
+
+  	$usid = @$_SESSION['identifier'] ?: null;
+  	unset($_SESSION['identifier']);
+
+  	$_SESSION['identifier'] = $_GET["openid_identity"];
+
+  	// Use a prepared statement
+  	$stmt = $mysqli->prepare("SELECT identifier FROM users WHERE identifier=?");
+
+  	if(!$stmt){
+  		printf("Query Prep Failed: %s\n", $mysqli->error);
+  		exit;
+  	}
+
+  	// Bind the parameter
+  	$user = $_GET['openid_identity'];
+  	$stmt->bind_param('s', $user);
+  	$stmt->execute();
+  	 
+  	// Bind the results
+  	$stmt->bind_result($user);
+  	if($stmt->fetch()) {
+  		$stmt->close();
+  		$stmt = $mysqli->prepare("INSERT INTO users (identifier) VALUES (?)");
+  		if(!$stmt){
+  			printf("Query Prep Failed: %s\n", $mysqli->error);
+  			exit;
+  		}
+
+  		$user = $_GET['openid_identity'];
+  		$stmt->bind_param('s', $user);
+  		$stmt->execute();
+
+  		$stmt->close();
+  	}
+
+  	exit;
+  ?>
+
+  //YAHOO.util.Connect.asyncRequest('GET', './openid_finish.php?'+openid_args,
+   //   {'success': function() {
+   //   		alert("success"); 
+   //   		document.getElementById("bucket").innerHTML = "SUCCESSSS";
+   //
+   //   		$("#ops").show();
+   //      }}); 
 }
 
 </script>
