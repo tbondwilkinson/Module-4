24622c1537c5679944119af29da3d215b3a02399
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-24 12:05:51 -0500

    Tom

--- Calendar/openid_finish.php
+++ Calendar/openid_finish.php
@@ -1,10 +1,42 @@
 <?php
-
+	
+	require "database.php";
 	session_start();
 
 	$usid = @$_SESSION['identifier'] ?: null;
 	unset($_SESSION['identifier']);
 
 	$_SESSION['identifier'] = $_GET["openid_identity"];
+
+	// Use a prepared statement
+	$stmt = $mysqli->prepare("SELECT identifier FROM users WHERE identifier=?");
+
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+
+	// Bind the parameter
+	$user = $_GET['openid_identity'];
+	$stmt->bind_param('s', $user);
+	$stmt->execute();
+	 
+	// Bind the results
+	$stmt->bind_result($user);
+	if($stmt->fetch()) {
+		$stmt->close();
+		$stmt = $mysqli->prepare("INSERT INTO users (identifier) VALUES (?)");
+		if(!$stmt){
+			printf("Query Prep Failed: %s\n", $mysqli->error);
+			exit;
+		}
+
+		$user = $_GET['openid_identity'];
+		$stmt->bind_param('s', $user);
+		$stmt->execute();
+
+		$stmt->close();
+	}
+
 	exit;
 ?>
