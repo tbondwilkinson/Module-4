c4caf83c380187918aaefed868351c9617a7dc1b
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-24 12:48:34 -0500

    t

---
+++ Calendar/events.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require "database.php";
+
+if(!isset($_SESSION['identifier'])) {
+	exit;
+}
+
+// Use a prepared statement
+$stmt = $mysqli->prepare("SELECT name, datetime FROM events WHERE owner=?");
+
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+// Bind the parameter
+$user = $_SESSION['identifier'];
+$stmt->bind_param('s', $user);
+$stmt->execute();
+
+
+$stmt->bind_result($name, $datetime);
+
+while($stmt->fetch()) {
+	echo "{\n";
+	echo "title: " . $name . "\n";
+	$php_date_str = substr($datetime,0,10).'T'.substr($datetime,11,8);
+	echo "start: new Date(" . $php_date_str . ")\n";
+	echo "}\n";
+
+}
+
+exit;
+$stmt->close;
+?>
--- Calendar/main.php
+++ Calendar/main.php
@@ -26,6 +26,13 @@ session_start();
 		var m = date.getMonth();
 		var y = date.getFullYear();
 		
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", "events.php", true);
+		xmlHttp.addEventListener("load", ajaxCallback false);
+		xmlHttp.send(null);
+	});
+
+	function ajaxCallback(event) {
 		$('#calendar').fullCalendar({
 			header: {
 				left: 'prev,next today',
@@ -34,54 +41,10 @@ session_start();
 			},
 			editable: true,
 			events: [
-				{
-					title: 'All Day Event',
-					start: new Date(y, m, 1)
-				},
-				{
-					title: 'Long Event',
-					start: new Date(y, m, d-5),
-					end: new Date(y, m, d-2)
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d-3, 16, 0),
-					allDay: false
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d+4, 16, 0),
-					allDay: false
-				},
-				{
-					title: 'Meeting',
-					start: new Date(y, m, d, 10, 30),
-					allDay: false
-				},
-				{
-					title: 'Lunch',
-					start: new Date(y, m, d, 12, 0),
-					end: new Date(y, m, d, 14, 0),
-					allDay: false
-				},
-				{
-					title: 'Birthday Party',
-					start: new Date(y, m, d+1, 19, 0),
-					end: new Date(y, m, d+1, 22, 30),
-					allDay: false
-				},
-				{
-					title: 'Click for Google',
-					start: new Date(y, m, 28),
-					end: new Date(y, m, 29),
-					url: 'http://google.com/'
-				}
+				event.target.responseText;
 			]
 		});
-		
-	});
+	}
 
 </script>
 <style type='text/css'>
