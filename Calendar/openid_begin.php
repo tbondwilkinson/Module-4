039190a73ea8864dc95e05e3947f6facb4835c86
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-23 21:36:51 -0500

    t

--- Calendar/openid_begin.php
+++ Calendar/openid_begin.php
@@ -2,11 +2,11 @@
 require_once 'OpenID/RelyingParty.php';
 require_once 'OpenID/Message.php';
 require_once 'Net/URL2.php';
- 
+
 session_start();
  
 $realm = "http://ec2-54-245-10-30.us-west-2.compute.amazonaws.com";
-$returnTo = $realm . "/~tbondwilkinson/Module-4/Calendar/popup_return_to.php"
+$returnTo = $realm . "/~tbondwilkinson/Module-4/Calendar/popup_return_to.php";
 
  
 $identifier = @$_GET['openid_identifier'] ?: @$_SESSION['identifier'] ?: null; // note: the @ signs suppress "undefined" notices
