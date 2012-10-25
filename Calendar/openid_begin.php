42340f9498f7cc0b63e31b1be7d8c9ddc5581575
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-25 16:14:43 -0500

    t

--- Calendar/openid_begin.php
+++ Calendar/openid_begin.php
@@ -9,7 +9,7 @@ $realm = "http://ec2-54-245-10-30.us-west-2.compute.amazonaws.com";
 $returnTo = $realm . "/~tbondwilkinson/Module-4/Calendar/popup_return_to.php";
 
  
-$identifier = @$_GET['openid_identifier'] ?: @$_SESSION['identifier'] ?: null; // note: the @ signs suppress "undefined" notices
+$identifier = $_GET['openid_identifier'];
  
 $o = new OpenID_RelyingParty($returnTo, $realm, $identifier);
 
