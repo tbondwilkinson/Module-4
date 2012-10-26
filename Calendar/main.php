f733e2bb24ed7507d842b50066143dda3e100fe7
tbondwilkinson <tbondwilkinson@gmail.com>
2012-10-25 20:52:25 -0500

    t

--- Calendar/main.php
+++ Calendar/main.php
@@ -48,7 +48,7 @@ function getEvents() {
 
 function ready() {
 	"use strict";
-	jQuery('#calendar').once('calendar').fullCalendar({
+	$('#calendar').fullCalendar({
 		header: {
 			left: 'prev,next today',
 			center: 'title',
